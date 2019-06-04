db3adc3f8da32f56994dfd75dcabe3f993dfe18a
samyakahuja <samyak.mcs18.du@gmail.com>
2019-06-04 19:47:15 +0530

    added process pools

--- hs_dl/__init__.py
+++ hs_dl/__init__.py
@@ -1,8 +1,10 @@
+from multiprocessing.pool import ThreadPool as Pool
 import os
 import re
 import requests
 from requests_html import HTMLSession
 import tempfile
+from urllib.parse import urlencode
 
 
 class HorribleDownloader:
@@ -12,6 +14,20 @@ class HorribleDownloader:
         self.verbose = verbose
         self.dry_run = dry_run
         self.temp_dir = tempfile.TemporaryDirectory(dir='/var/tmp')
+        self.api_url = 'https://horriblesubs.info/api.php?'
+        self.torrent_links = []
+        self.workers = 8
+
+
+    def download_torrent(self, link):
+        if self.dry_run:
+           print(f"[ ] Would Download {link['torrent_file_name']} at {link['torrent_file_path']}]") 
+           return
+        res = requests.get(link['torrent_link'])
+        with open(link['torrent_file_path'], 'wb') as f:
+            f.write(res.content)
+            print(f"[+] Downloaded {link['torrent_file_path']}")
+   
 
     def download_torrents(self):
         session = HTMLSession()
@@ -27,8 +43,25 @@ class HorribleDownloader:
 
         # next_id is defined beacuse of pagination
         next_id = 0
+        
+        print('[ ] Collecting Torrent Info...')
         while True:
-            torrent_url = 'https://horriblesubs.info/api.php?method=getshows&type=show&showid={}&nextid={}'.format(show_id, next_id)
+
+            query_params = {
+                'method': 'getshows',
+                'type': 'show',
+                'showid': show_id,
+                'nextid': next_id
+            }
+
+            query_string = urlencode(query_params)
+            torrent_url = self.api_url + query_string
+
+            if self.verbose:
+                print(f'[*] Acting on {torrent_url}')
+            else:
+                print(f'[ ] Accessing page {next_id}')
+
             response = session.get(torrent_url)
 
             if response.status_code == 200 or response.status_code < 300:
@@ -39,7 +72,7 @@ class HorribleDownloader:
                 return
 
             if response.text == 'DONE':
-                print('[+]', response.text)
+                print('[ ] Done Accessing pages')
                 break
 
             episode_elements = response.html.find('.rls-info-container')
@@ -52,18 +85,35 @@ class HorribleDownloader:
                     torrent_file_path = os.path.join(self.temp_dir.name, torrent_file_name)
                     torrent_link = link_container.find('.hs-torrent-link', first=True).find('a', first=True).attrs['href']
 
-                    res = requests.get(torrent_link)
-                    if self.dry_run:
-                       print(f'[*] Would Download {torrent_file_name} at {torrent_file_path}]') 
-                    else:
-                        with open(torrent_file_path, 'wb') as f:
-                            f.write(res.content)
-                            print(f'[+] Downloaded {torrent_file_path}')
+                    torrent_info = {
+                        'torrent_link': torrent_link,
+                        'torrent_file_name': torrent_file_name,
+                        'torrent_file_path': torrent_file_path
+                    }
+
+                    self.torrent_links.append(torrent_info)
 
                 except Exception as e:
                     print(e)
 
             next_id += 1
+
+        if self.dry_run:
+            print('\n[ ] Starting Emulation of Downloading')
+        else:
+            print('\n[ ] Starting Downloading')
+        print('[ ] NOTE: Downloaded files may appear out of order.')
+
+        # create thread pool
+        pool = Pool(self.workers)
+        for result in pool.imap_unordered(self.download_torrent, self.torrent_links):
+            pass
+
+        if self.dry_run:
+            print('[ ] Finished Emulation of Downloading')
+        else:
+            print('[ ] Finished Downloading')
+
 
     def run(self):
         self.download_torrents()
